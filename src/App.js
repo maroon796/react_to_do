8246c91e8517f9813f77b50e0c53e25e09fd669f
Grygory Ksheminsky <maroon785@gmail.com>
2022-02-21 14:20:28 +0200

    start working with App-file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
+import ToDo from './ToDo';
+import Form from './Form';
 
 function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
   return <div className="App"></div>;
 }
 
